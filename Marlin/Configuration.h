260198662176a95ec462b87a5c8cba8427c86943
Spencer Bertram <Spenbert02@gmail.com>
2022-08-30 13:26:13 -0500

    proper bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1376,8 +1376,8 @@
 #define X_MIN_POS -6  // changed from 0, because custom fan shroud has offset off build plate
 #define Y_MIN_POS -13  // changed, same reason as x_min_pos change
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + X_MIN_POS  // changed to keep x range at 230
+#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS  // changed to keep y range at 230
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
